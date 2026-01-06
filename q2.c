3eded9da966d371cd63e4649317490b259018717
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-01-06 23:48:08 +0530

    learning c

--- q2.c
+++ q2.c
@@ -2,5 +2,5 @@
 #include<stdio.h>
 void main()
 {
-    printf("Name = Alexei Sytsevich\nAddress = 135 E 57th Street, New York\nPin code = NY 10022\nHome = OsCorp");
+    printf("Name = Alexei Sytsevich\nAddress = 135 E 57th Street, New York\nPin code = NY 10022\nHome = OsCorp\n");
 }
