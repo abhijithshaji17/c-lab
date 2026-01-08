bbff32ccaecefe804f5d55a17cd66c15c122cbe1
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-01-08 20:20:22 +0530

    lab work

---
+++ q4.c
@@ -0,0 +1,20 @@
+/*
+Name = Abhijith Shaji
+Roll No. = 25020144
+Department = Department of Computer Science(DCS)
+*/
+
+// Q4. Write a C program to print sum of 2 variables
+
+#include<stdio.h>
+void main()
+{
+    int a,b,sum;
+    a = 20;
+    b = 30;
+    sum = a + b;
+    printf("%d\n", sum);
+    printf("sum is %d\n", sum);
+    printf("sum of a and b is %d\n", sum);
+    printf("sum of %d and %d is %d\n",a,b,sum);
+}
