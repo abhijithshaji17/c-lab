9cfb7bb25edd3e66898571a9e0346f1c03b88572
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-01-08 19:57:20 +0530

    lab work

---
+++ q3.c
@@ -0,0 +1,20 @@
+/*
+Name = Abhijith Shaji
+Roll No. = 25020144
+Department = Department of Computer Science(DCS)
+*/
+
+/*
+Q3. Write a C program to display the pattern:
+         1
+         1  2
+         1  2  3
+*/ 
+
+#include<stdio.h>
+void main()
+{
+    printf("1\n");
+    printf("1 2\n");
+    printf("1 2 3\n");
+}
