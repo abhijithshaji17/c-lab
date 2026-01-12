0b2dc631d357e4c3e1efeb8ff78c8ab9eed96160
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-01-12 21:48:55 +0530

    lab work

---
+++ q5.c
@@ -0,0 +1,21 @@
+/*
+Name = Abhijith Shaji
+Roll No. = 25020144
+Department = Department of Computer Science(DCS)
+*/
+
+/*
+Write a program to display the equation of a line in the form
+ax + by = c
+for a = 5, b = 8 and c = 18.
+*/
+
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    a = 5;
+    b = 8;
+    c = 18;
+    printf("%dx + %dy = %dc", a,b,c);
+}
