4d305789862e6bfa85973391e6075e4aedf3a183
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-01-13 20:06:10 +0530

    lab work

---
+++ q7.c
@@ -0,0 +1,20 @@
+/*
+Name = Abhijith Shaji
+Roll No. = 25020144
+Department = Department of Computer Science(DCS)
+*/
+
+//Write a C program to read length and breadth from user, calculate the area of a rectangle and display it.
+
+#include<stdio.h>
+int main()
+{
+    int l,b,ar;
+    printf("Input length(in units) = ","%d\n", l);
+    scanf("%d", &l);
+    printf("Input breadth(in units) = ","%d\n", b);
+    scanf("%d",&b);
+    ar = l*b;
+    printf("Area of rectangle(in sq.units) = %d\n", ar);
+    return 0;
+}
