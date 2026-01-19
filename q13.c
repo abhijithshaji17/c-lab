dee8b7d646373ae20c38afafa4b4850206bc941f
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-01-19 20:55:01 +0530

    lab work

---
+++ q13.c
@@ -0,0 +1,14 @@
+/*
+Name = Abhijith Shaji
+Roll No. = 25020144
+Department = Department of Computer Science(DCS)
+*/
+
+/*  
+The straight-line method of computing the yearly depreciation of
+the value of an item is given by:- 
+        depreciation = (purchase price - salvage value)/years of service
+Write a program to determine the salvage value of an item when
+the purchase price, years of service, and the annual depreciation
+are given.
+*/
