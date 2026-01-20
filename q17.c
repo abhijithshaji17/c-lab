b1a366b963dad73bf5a9847b16f91e1f8cf5e2dc
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-01-20 20:42:01 +0530

    lab work

--- q17.c
+++ q17.c
@@ -10,4 +10,13 @@ The relationship between the 2 temperaure scales is:-
         c/5 = (f-32)/9
 */ 
 
-
+#include<stdio.h>
+int main()
+{
+        double c,f;
+        printf("Enter the temperature in Celsius = ");
+        scanf("%lf",&c);
+        f = (9.0/5.0)*c + 32.0;
+        printf("Equivalent Fahrenheit temperature = %lf\n",f);
+        return 0;
+}
