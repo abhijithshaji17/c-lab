0355aaae152cf55a1cfab3f51ab20607160f832c
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-01-20 19:11:01 +0530

    lab work

---
+++ q17.c
@@ -0,0 +1,13 @@
+/*
+Name = Abhijith Shaji
+Roll No. = 25020144
+Department = Department of Computer Science(DCS)
+*/
+
+/*
+Write a C program to obtain equivalent fahrenheit temperature, when degree celsius is given as input.
+The relationship between the 2 temperaure scales is:-
+        c/5 = (f-32)/9
+*/ 
+
+
