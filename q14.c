0c1bb3c92fa2b91bbd87b7ad8b7d7bcaf3184ea4
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-01-20 09:58:37 +0530

    lab work

---
+++ q14.c
@@ -0,0 +1,14 @@
+/*
+Name = Abhijith Shaji
+Roll No. = 25020144
+Department = Department of Computer Science(DCS)
+*/
+
+/*  
+In inventory management, the Economic Order Quantity for a
+single item is given by:-
+    eoq = sqrt((2*demand rate*setup costs)/holding costs per item per unit time)
+
+and the optimal Time Between Orders:-
+    tbo = sqrt((2*setup costs)/demand rate*holding costs per unit time)
+*/
