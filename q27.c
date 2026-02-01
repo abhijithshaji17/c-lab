2eb046e5a57331773794da042656b77d8ef5c81d
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-02-01 12:12:43 +0530

    lab work

---
+++ q27.c
@@ -0,0 +1,19 @@
+/*
+Name = Abhijith Shaji
+Roll No. = 25020144
+Department = Department of Computer Science(DCS)
+*/
+
+//Write a C program to print "Hello" 10 times using a for loop. 
+
+#include<stdio.h>
+int main()
+{
+    int n,i = 1;
+    printf("Enter the number of times to print: ");
+    scanf("%d",&n);
+    for(i = 1; i <= n; i++){
+        printf("Hello\n");
+    }
+    return 0;
+}
