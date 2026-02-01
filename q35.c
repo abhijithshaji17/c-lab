1b380723e884f4cbd6a32e973b8fbf52492c3704
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-02-01 20:58:49 +0530

    lab work

---
+++ q35.c
@@ -0,0 +1,10 @@
+/*
+Name = Abhijith Shaji
+Roll No. = 25020144
+Department = Department of Computer Science(DCS)
+*/
+
+/* 
+Write a program to find the number of and sum of all integers greater than 100 and less than 200 that are divisible by 7.
+*/
+
