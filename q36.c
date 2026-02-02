bfe76ce11ab5351f553e59ccb18a262a0e0a026a
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-02-02 20:48:34 +0530

    lab work

---
+++ q36.c
@@ -0,0 +1,17 @@
+/*
+Name = Abhijith Shaji
+Roll No. = 25020144
+Department = Department of Computer Science(DCS)
+*/
+
+/* 
+A set of two linear equations with two unknowns x1 and x2 is given below:
+ax1 + bx2 = m
+cx1 + dx2 = n
+The set has a unique solution:
+x1 = (md - bn)/(ad - cb)
+x2 = (na - mc)/(ad - cb)
+provided the denominator ad - cb != 0. Write a program that will read the values of constants a,b,c,d,m and n and compute the values of x1 and x2. An appropriate message should be printed if ad - cb = 0. 
+*/
+
+
