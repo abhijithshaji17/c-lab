5ba798cbb9f95dcf8936997104e3591ccd27fca4
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-02-02 17:59:38 +0530

    lab work

--- q35.c
+++ q35.c
@@ -5,6 +5,6 @@ Department = Department of Computer Science(DCS)
 */
 
 /* 
-Write a program to find the number of and sum of all integers greater than 100 and less than 200 that are divisible by 7.
+Write a C program to find the number of and sum of all integers greater than 100 and less than 200 that are divisible by 7.
 */
 
