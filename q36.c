c4a4674ba9568e1239b23d33f71bb0e93b5cc516
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-02-03 17:00:28 +0530

    lab work

--- q36.c
+++ q36.c
@@ -14,4 +14,20 @@ x2 = (na - mc)/(ad - cb)
 provided the denominator ad - cb != 0. Write a program that will read the values of constants a,b,c,d,m and n and compute the values of x1 and x2. An appropriate message should be printed if ad - cb = 0. 
 */
 
-
+#include<stdio.h>
+int main()
+{
+    double a,b,c,d,m,n,x1,x2;
+    printf("Enter the respective values:\n");
+    scanf("%lf %lf %lf %lf %lf %lf",&a,&b,&c,&d,&m,&n);
+    x1 = (m*d - b*n)/(a*d - c*b);
+    x2 = (n*a - m*c)/(a*d - c*b);
+    if((a*d - c*b) == 0){
+        printf("Error: division by zero!");
+    }
+    else{
+        printf("x1 = %lf\n", x1);
+        printf("x2 = %lf\n", x2);
+    }
+    return 0;
+}
