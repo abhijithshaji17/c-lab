bfeb26d7d7b04559e67e93d64daea7637fce1f6b
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-02-03 20:52:52 +0530

    lab work

---
+++ q39.c
@@ -0,0 +1,13 @@
+/*
+Name = Abhijith Shaji
+Roll No. = 25020144
+Department = Department of Computer Science(DCS)
+*/
+
+/* 
+Write a program to compute the real roots of a quadratic equation:
+ax2 + bx + c = 0
+The roots are given by the equations:
+x = (-b+-sqrt(b2-4ac))/2a
+*/
+
