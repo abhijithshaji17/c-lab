d50c0e1feedf71a6bb5de012fd666d76a3dd613d
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-02-03 17:14:52 +0530

    lab work

---
+++ q37.c
@@ -0,0 +1,18 @@
+/*
+Name = Abhijith Shaji
+Roll No. = 25020144
+Department = Department of Computer Science(DCS)
+*/
+
+/* 
+Given a list of marks ranging from 0 to 100, write a program to compute and print the number of students:
+(a) who have obtained more than 80 marks,
+(b) who have obtained more than 60 marks,
+(c) who have obtained more than 40 marks,
+(d) who have obtained 40 or less marks,
+(e) in the range 81 to 100,
+(f) in the range 61 to 80,
+(g) in the range 41 to 60, and
+(h) in the range 0 to 40. The program should use a minimum number of if statements.
+*/
+
