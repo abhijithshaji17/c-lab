3649f8e17485138914f1b93b6b29579b57545e7e
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-02-04 22:15:19 +0530

    lab work

---
+++ q41.c
@@ -0,0 +1,41 @@
+/*
+Name = Abhijith Shaji
+Roll No. = 25020144
+Department = Department of Computer Science(DCS)
+*/
+
+/* Break &*/
+
+// #include<stdio.h>
+// int main()
+// {
+//     int i,j;
+//         for(i=0;i<=5;i++){
+//             for(j=1;j<=i;j++){
+//                printf("%d ",j);
+//             }
+//            if(i==j){
+//                break;
+//             }
+//             printf("%d ",i);
+//             printf("\n");
+//         }
+//         return 0;
+// }
+
+#include<stdio.h>
+int main()
+{
+    int i,j;
+        for(i=0;i<=5;i++){
+            for(j=1;j<=i;j++){
+               printf("%d ",j);
+            }
+           if(i==j){
+               continue;
+            }
+            printf("%d ",i);
+            printf("\n");
+        }
+        return 0;
+}
