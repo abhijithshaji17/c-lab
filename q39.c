765b2cf1837876d1d502b792c8001a54aab3c1a1
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-02-04 14:25:52 +0530

    lab work

--- q39.c
+++ q39.c
@@ -11,3 +11,31 @@ The roots are given by the equations:
 x = (-b+-sqrt(b2-4ac))/2a
 */
 
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    double a,b,c,d,r1,r2;
+    printf("Enter the coefficients of a,b and c: ");
+    scanf("%lf %lf %lf",&a,&b,&c);
+    if(a == 0){
+        printf("Error: 'a' can't be 0\n");
+        return 1;
+    }
+    d = b*b - 4*a*c;
+    if(d > 0){
+        r1 = -b + sqrt(b*b - 4*a*c)/2*a;
+        r2 = -b - sqrt(b*b - 4*a*c)/2*a;
+        printf("The roots are real and distinct: %.2lf & %.2lf",r1,r2);
+    }
+    else if(d == 0){
+        r1 = r2 = -b/(2*a);
+        printf("The roots are real and equal: %.2lf & %.2lf",r1,r2);
+    }
+    else{
+        double realpart = -b/(2*a);
+        double imagpart = sqrt(-d)/(2*a); 
+        printf("Roots are complex: %.2lf + %.2lfi and %.2lf - %.2lfi\n",realpart, imagpart, realpart, imagpart);
+    }
+    return 0;
+}
