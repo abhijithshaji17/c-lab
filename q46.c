a1c3d8b750c98fcddca7151eee20daff9d12c06f
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-02-07 22:17:32 +0530

    lab work

--- q46.c
+++ q46.c
@@ -4,4 +4,25 @@ Roll No. = 25020144
 Department = Department of Computer Science(DCS)
 */
 
-/*Palindrome*/
+/*Palindrome number*/
+
+#include<stdio.h>
+int main()
+{
+    int n,rev=0,rem,org;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    org = n;
+    while(n!=0){
+        rem = n%10;
+        rev = rev*10+rem;
+        n /= 10;
+    }
+    if(org==rev){
+        printf("%d is a palindromic number",org);
+    }
+    else{
+        printf("%d is not a palindromic number",org);
+    }
+    return 0;
+}
